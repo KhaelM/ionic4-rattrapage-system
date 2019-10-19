afd6204ee7c4f5c65cd38f48afaa96d41894aae8
KhaelM <michael.randrianarisona@outlook.com>
2019-10-19 21:47:11 +0300

    firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCPQb3haBOlA7iyb1XmeTI5pP1lAMqbIGE",
+    authDomain: "ionic-firebase-e3d08.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-e3d08.firebaseio.com",
+    projectId: "ionic-firebase-e3d08",
+    storageBucket: "ionic-firebase-e3d08.appspot.com",
+    messagingSenderId: "808931971642"
+  }
 };
 
 /*
